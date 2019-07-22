4da36ea7a042033fb30ccf644772440450a93a32
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-07-22 13:54:06 +0530

    creating another line

--- tickTackToe.py
+++ tickTackToe.py
@@ -6,6 +6,7 @@ gameboard=Canvas(window,width=width,height=height)
 gameboard.create_line(0,height/3,width,height/3)
 gameboard.create_line(0,(height/3)*2,width,(height/3)*2)
 gameboard.create_line(width/3,0,width/3,height)
+gameboard.create_line((width/3)*2,0,(width/3)*2,height)
 gameboard.pack()
 
 
